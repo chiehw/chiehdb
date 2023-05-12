5b7474a4d3fcb950cac8bf96bdf2ff7ccc30a13a
chieh <todaymofish@gmail.com>
2023-05-12 00:38:30 -0700

    Create db by DB::Open

---
+++ db/db_impl.cc
@@ -0,0 +1,50 @@
+#include "db/db_impl.h"
+#include "leveldb/db.h"
+#include "db/filename.h"
+
+namespace leveldb
+{
+    DB::~DB() {}
+
+    DBImpl::DBImpl(const Options &raw_options, const std::string &dbname)
+        : dbname_(dbname), env_(raw_options.env)
+    {
+        versions_ = new VersionSet();
+    }
+
+    Status DB::Open(const Options &options, const std::string &dbname, DB **dbptr)
+    {
+        *dbptr = NULL;
+
+        DBImpl *impl = new DBImpl(options, dbname);
+        impl->mutex_.Lock();
+
+        // 通过 log 文件恢复 MemTable
+        impl->env_->CreateDir(dbname);
+        // 创建 Log 文件
+        WritableFile *lfile;
+        uint64_t new_log_number = impl->versions_->NewFileNumber();
+        Status s = options.env->NewWritableFile(LogFileName(dbname, new_log_number), &lfile);
+
+        if (s.ok())
+        {
+            impl->logfile_number_ = new_log_number;
+            impl->logfile_ = lfile;
+            impl->log_ = new log::Writer(lfile);
+        }
+
+        impl->mutex_.Unlock();
+        if (s.ok())
+        {
+            *dbptr = impl;
+        }
+        else
+        {
+            delete impl;
+        }
+        return s;
+    }
+    DBImpl::~DBImpl()
+    {
+    }
+}
---
+++ db/db_impl.h
@@ -0,0 +1,37 @@
+#ifndef STORAGE_LEVELDB_DB_DB_IMPL_H_
+#define STORAGE_LEVELDB_DB_DB_IMPL_H_
+
+#include "leveldb/options.h"
+#include "leveldb/db.h"
+#include "port/port.h"
+#include "leveldb/status.h"
+#include "leveldb/env.h"
+#include "version_set.h"
+#include "db/log_write.h"
+
+namespace leveldb
+{
+    class DBImpl : public DB
+    {
+    public:
+        DBImpl(const Options &options, const std::string &dbname);
+        virtual ~DBImpl();
+
+    private:
+        friend class DB;
+        const std::string dbname_;
+        Env *const env_;
+
+        uint64_t logfile_number_;
+        WritableFile *logfile_;
+        log::Writer *log_;
+
+        port::Mutex mutex_;
+        VersionSet *versions_;
+
+        DBImpl(const DBImpl &);
+        void operator=(const DBImpl &);
+    };
+}
+
+#endif
