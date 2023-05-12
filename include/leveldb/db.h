8cfc795d5f796b0af5cbe1e76954346ae00712ff
chieh <todaymofish@gmail.com>
2023-05-12 00:19:59 -0700

    Open to init db

---
+++ include/leveldb/db.h
@@ -0,0 +1,25 @@
+#ifndef STORAGE_LEVELDB_INCLUDE_DB_H_
+#define STORAGE_LEVELDB_INCLUDE_DB_H_
+
+#include <string>
+#include "leveldb/status.h"
+#include "leveldb/options.h"
+#include "leveldb/write_batch.h"
+
+namespace leveldb
+{
+    class DB
+    {
+    public:
+        DB(){};
+        virtual ~DB();
+        /// 使用案例
+        /// Db* db;
+        /// Status status = leveldb::DB::Open(options, "/tmp/testdb", &db_);
+        static Status Open(const Options &options,
+                           const std::string &name,
+                           DB **dbptr);
+    };
+
+}
+#endif
